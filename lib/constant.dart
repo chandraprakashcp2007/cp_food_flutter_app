b9575a36adb9c39a9af6cdfe9fd381fcfe70472c
Chandra Prakash <chandraprakashcp2007@gmail.com>
2025-11-23 15:30:19 +0530

    dummy removed

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,4 +3,3 @@ const String slogan = "Delivery App";
 const String bgImage = "assets/food_bg.jpg";
 const String loginString = "LOGIN";
 const String forgetText="Forget Password ?";
-const String dummyString = "Dummy";
