8f9514aa1d5821ee223f37f179c64d5ef8658a46
Chandra Prakash <chandraprakashcp2007@gmail.com>
2025-11-23 15:25:05 +0530

    dummy check

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,4 @@ const String slogan = "Delivery App";
 const String bgImage = "assets/food_bg.jpg";
 const String loginString = "LOGIN";
 const String forgetText="Forget Password ?";
+const String dummyString = "Dummy";
